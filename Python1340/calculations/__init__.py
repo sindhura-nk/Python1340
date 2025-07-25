bb8bda22ac0bb8f07d72216d21a70b6394dc5398
sindhura-nk <nk.sindhura@gmail.com>
2025-07-25 16:14:29 +0530

    init file updated

--- Python1340/calculations/__init__.py
+++ Python1340/calculations/__init__.py
@@ -1,2 +1,2 @@
-def __init__(self):
-    pass
+# init is used to initialise the packages
+# init.py is a mandatory module while defining the packages. Always ensure that you create this file whenever you want to create packages.
