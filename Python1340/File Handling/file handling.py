5dc616a7593fe637ae53db00dfe2b1b3afb7cca7
sindhura-nk <nk.sindhura@gmail.com>
2025-07-25 11:55:34 +0530

    file handling practical

--- Python1340/File Handling/file handling.py
+++ Python1340/File Handling/file handling.py
@@ -25,37 +25,36 @@ print(f2.read())
 f2.close()
 
 
-# os.chdir(r"E:\Sindhura\DataSets")
-# with open("FH-sampledata.txt",'r') as file:
-#     r1 = file.read()
-#     print(f"Read():{r1}")
-# with open("FH-sampledata.txt",'r') as file:
-#     r2 = file.readline()
-#     print(f"Readline():{r2}")
-# with open("FH-sampledata.txt",'r') as file:
-#     r3 = file.readlines()
-#     print(f"Readlines():{r3}")
+os.chdir(r"E:\Sindhura\DataSets")
+with open("FH-sampledata.txt",'r') as file:
+    r1 = file.read()
+    print(f"Read():{r1}")
+with open("FH-sampledata.txt",'r') as file:
+    r2 = file.readline()
+    print(f"Readline():{r2}")
+with open("FH-sampledata.txt",'r') as file:
+    r3 = file.readlines()
+    print(f"3rd and 5th Line using readlines() :{r3[2]},{r3[4]}")
+    print(f"Readlines():{r3}")
 
-# hint while reading the data: hint represents number of bytes 
+#hint while reading the data: hint represents number of bytes 
 
-# with open("FH-sampledata.txt",'r') as file:
-#     r1 = file.read(20)
-#     print(f"Read():{r1}")
-# with open("FH-sampledata.txt",'r') as file:
-#     r2 = file.readline(20)
-#     print(f"Readline():{r2}")
-# with open("FH-sampledata.txt",'r') as file:
-#     r3 = file.readlines(1000)
-#     print(f"Readlines():{r3}")
-
-
-
+with open("FH-sampledata.txt",'r') as file:
+    r1 = file.read(20)
+    print(f"Read():{r1}")
+with open("FH-sampledata.txt",'r') as file:
+    r2 = file.readline(20)
+    print(f"Readline():{r2}")
+with open("FH-sampledata.txt",'r') as file:
+    r3 = file.readlines(1000)
+    print(f"Readlines():{r3}")
 
 
 # How to write to files? => using write(),writelines()
 f3 = open("sample.txt","w")
 f3.write("Welcome to the Python session.")
 f3.close()
+
 
 # How to append data to existing files: => using write() and choosing 'a' as the mode
 f4 = open("sample.txt","a")
@@ -65,37 +64,27 @@ f4.close()
 # read the content after updating the data
 f5 = open("sample.txt","r")
 print(f5.read())
+f5.close()
 
 # How to close the files
+#using filename.close()
 
 # With clause: why and advantages
+''' 
+with open(file_name,mode) as file:
+    file.read()
+'''
+with open("sample.txt","r") as file:
+    print("With clause output",file.read())
 
-# remove a file
+with open("sample.txt","a") as file:
+    file.write("\n Today's topic is File Handling")
 
-# create directory(folder)
+with open("sample.txt","r") as file:
+    print("With clause output",file.read())
 
-# remove directory (os.rmdir removes only an empty directory)
+# os.getcwd()
+os.chdir(r"E:\Sindhura\Sindhura\Data Science-ETLhive\Files\emoji\happy")
+with open("ffhq_1740.png","rb") as file:
+    print(file.read())
 
-# change the directory path
-
-# create a file within the directory
-
-# change the directory path
-
-# try to remove the directory where you have created a new file
-
-# read an image file : mode = 'rb'
-
-# Problem Statement: write a program that accepts a string from the user and writes it to a text file. 
-# Thereafter, the same program reads the text file and displays it on the screen.
-
-# print("Seek and Tell methods")
-# # seek() and tell()
-# # If we want to get the position of the fileobject
-# with open("FH-sampledata.txt",'+r') as file:
-#     data = file.read()
-#     print(f"Initial position of the file object is : {file.tell()}")
-#     file.seek(0)
-#     print(f"Now the file object is at the beginning of the file: {file.tell()}")
-#     file.seek(10)
-#     print(f"Initial position of the file object is : {file.tell()}")
