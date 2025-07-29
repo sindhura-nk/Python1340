b480ff88cc7df4902d5e025336868c08001ed1f2
sindhura-nk <nk.sindhura@gmail.com>
2025-07-29 11:47:27 +0530

    Inheritance-done

--- Python1340/OOPS/Inheritance.py
+++ Python1340/OOPS/Inheritance.py
@@ -62,12 +62,88 @@ print("Parent class object output")
 animal1 = Animal("Cat")
 animal1.animal_sound()
 
-
-
 # Multi level Inheritance
+print("=============================================")
+print("MULTI LEVEL INHERITANCE")
+class Employee:
+    def __init__(self,name,id):
+        self.name = name
+        self.id = id
+    def Employee_Data(self):
+        print(f"Employee ID of {self.name} is {self.id}")
 
+class Manager(Employee):
+    def __init__(self, name, id,team_name):
+        super().__init__(name, id)
+        self.team_name = team_name
+    def Manager_Info(self):
+        #super().Employee_Data()
+        print(f"This employee works in {self.team_name}")
 
+class Project_Manager(Manager):
+    def __init__(self, name, id, team_name,project_name):
+        super().__init__(name, id, team_name)
+        self.project_name = project_name
+    def Project_Data(self):
+        super().Employee_Data()
+        super().Manager_Info()
+        print(f"This Employee now got promoted and works in {self.project_name}")
 
+# Employee object
+print("Employee object Output: ")
+e1= Employee("Raman",102)
+e1.Employee_Data()
+
+print("Manager Object Output: ")
+m1 = Manager("Suman",145,"E2K")
+m1.Manager_Info()
+
+print("Project Manager Output")
+p1 = Project_Manager("Thaman",190,"C3K","UPS")
+p1.Project_Data()
+p1.Employee_Data()
+p1.Manager_Info()
 
 
 # Hierarchal Inheritance
+print("============================")
+print("Hierarchal Output")
+class Showroom:
+    def __init__(self,showroom_name,branch):
+        self.showroom_name = showroom_name
+        self.branch = branch
+    def greeting(self):
+        print(f"Welcome to our showroom {self.showroom_name} located at {self.branch}")
+class Model1(Showroom):
+    def __init__(self, showroom_name, branch,price_range):
+        super().__init__(showroom_name, branch)
+        self.price_range = price_range
+    def Model1_Details(self):
+        super().greeting()
+        # price_range = int(input("Please enter your price range in a single number format. Ex:12"))
+        # when you are using input(), in this scenario donot use price_range as instance attribute
+        if 10 < self.price_range <15:
+            print(f"Details of the car are : Grand i10. Colors available is Red")
+        elif 0 < self.price_range >15:
+            print(f"For this range, we are providing a high end model-Grand i20. Kindly let us know if you are interested")
+        else:
+            print("Sorry. Our car range starts from 10.5L")
+class Model2(Showroom):
+    def __init__(self, showroom_name, branch,price_range):
+        super().__init__(showroom_name, branch)
+        self.price_range = price_range
+    def Model2_Details(self):
+        if self.price_range >15:
+            print(f"Details of the car are : Grand i20. Colors available is Red")
+        elif 10< self.price_range <15:
+            print(f"For this range, we are providing a high end model-Grand i10. Kindly let us know if you are interested")
+        else:
+            print("Sorry. Our car range starts from 10.5L")
+
+print("Model1 output")
+m1_1 = Model1("Garve Hyundai","Wakad",20) 
+m1_1.Model1_Details()
+print("Model2 Output")
+m2_1 = Model2("Garve Hyundai","Ravet",19)
+m2_1.Model2_Details()
+        
